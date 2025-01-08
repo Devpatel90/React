0dbe191ca5ba82b6336a88d9b473205fa0e3d545
Devpatel90 <devsojitra02@gmail.com>
2025-01-08 22:23:37 +0530

    File Uploaded

--- Day-4/src/Components/Products.jsx
+++ Day-4/src/Components/Products.jsx
@@ -1,10 +1,9 @@
 import { useState , useEffect } from 'react'
+import styles from './Prod.module.css'
 
 function Products() {
 
-
-    function app(){
-        const [pro,setProd] = useState(null)
+        const [prod,setProd] = useState(null)
 
         useEffect(()=>{
             fetch(`https://dummyjson.com/products`)
@@ -12,25 +11,39 @@ function Products() {
             .then((Res)=>{
                 console.log(Res.products);
                 setProd(Res.products)
+                console.log(prod);
+                
             })
             .catch((err)=>{
             console.log(err)
             })
        },[])
-    }
+
+
   return (
- <>
- <h1>Product List</h1>
-    {
-        data.map((el)=>{
-             return <div>
-                   <img src={el.thumbnail} alt={el.title} />
-                   <p>{el.title}</p>
-             </div>
-        })
-      }
- </>
+    <div>
+        <div className='d-flex justify-content-evenly gap-5 my-4 flex-wrap'>
+        {
+          prod == null
+          ?
+          <h1>Loading...</h1>
+          :
+          prod.map((el)=>{
+              return(
+                <>
+                  <div className={styles.box}>
+                    <img src={el.thumbnail} alt="" />
+                    <h1 className='fw-bold mt-3 fs-5 m-auto'>{el.title}</h1>
+                    <p className='m-auto mt-2'>{el.brand} ({el.category})</p>
+                    <p className='fs-5 mt-2 m-auto'><span className='text-success'></span> &#8377;{el.price}</p>
+                  </div>
+                </>
+              )
+          })
+        } 
+        </div>
+  </div>
   )
 }
 
-export default Products
+export default Products;
